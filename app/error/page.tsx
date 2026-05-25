af2fccfa25e374332b79429a615cbb61fd54c577
Yadhuveer <yadhuveeryadu@gmail.com>
2026-05-25 23:43:00 +0530

    rediis added

--- app/error/page.tsx
+++ app/error/page.tsx
@@ -4,7 +4,7 @@ export default function ErrorPage() {
       <div className="bg-white shadow-lg rounded-xl p-8 max-w-md text-center">
         <h1 className="text-3xl font-bold text-red-600 mb-4">Server Error</h1>
         <p className="text-gray-700 mb-4">
-          We’re having some trouble right now. Please try again laterr.
+          We’re having some trouble right now. Please try again laterrr.
         </p>
         <a
           href="/"
